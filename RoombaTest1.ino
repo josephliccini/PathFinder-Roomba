69507296d704e2dbbcd366b0202e14f1597f48ce
Joseph Liccini <joseph.liccini@ufl.edu>
2016-03-31 19:16:35 -0400

    Roomba is Moving

---
+++ RoombaTest1.ino
@@ -0,0 +1,29 @@
+#include <Roomba.h>
+
+Roomba roomba(&Serial1, Roomba::Baud19200);
+int ledPin =  13;
+
+void setup()
+{    
+    pinMode(ledPin, OUTPUT);  
+    roomba.start();
+
+    roomba.fullMode();
+}
+
+void loop()
+{
+  roomba.leds(ROOMBA_MASK_LED_ADVANCE, 255, 255);
+
+  roomba.drive(100, 0);
+
+  delay(700);
+  digitalWrite(ledPin, HIGH);
+  
+  roomba.leds(ROOMBA_MASK_LED_ADVANCE, 0, 255);
+
+  roomba.drive(-100, 0);
+  
+  delay(700);
+  digitalWrite(ledPin, LOW);
+}
